a30593740a6ce359eea33acee29f644d9fa2f328
William <87915329+westliam-x@users.noreply.github.com>
2022-12-30 22:22:28 +0100

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import avatar from "../public/avatar.png"
-import westliam2 from '../public/westliam2.png';
 function About(){
     return(
-    <div className="relative m-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-90 md:w-90" >
+    <div className="relative mx-auto bg-gradient-to-b from-blue-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96" >
        <Image src={avatar} layout="fill" objectFit="cover" />
     </div>
        
