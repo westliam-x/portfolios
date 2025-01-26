94352a50e3ef6a85a5566a11be1066e059b95c53
william <emelifonwuw@gmail.com>
2025-01-26 17:32:08 +0100

    refactor: improve formatting and structure in Card component

--- pages/card.js
+++ pages/card.js
@@ -1,61 +1,74 @@
 import { HiOutlineDocumentText } from "react-icons/hi";
 import { MdOutlineDesignServices } from "react-icons/md";
 import { BsCodeSlash } from "react-icons/bs";
-function Card(){
-    return(
-
+function Card() {
+  return (
     <div className="lg:flex gap-16">
-    
-    <div style={{ 
-        boxShadow: '1px 0px 10px 3px #d6d6e8',
-    }} 
-    className=" text-center dark:shadow-slate-400 transition-shadow p-10 rounded-xl my-10  dark:text-white">
-        <div>  
-        <MdOutlineDesignServices className=" m-auto" size={70}/>
-       <h3 className=" text-lg font-medium pt-8 pb-2">Exquisite UI Designs</h3>
-       <p className="py-2">Creating beautiful UI designs to satisfy your needs and the needs of your clients</p>
-        <h4 className="text-teal-500 py-4">Design tools I use:</h4>
-        <div className='flex  m-1 items-center space-x-6 justify-center'>
-        <p className="text-grey-800 py-1"> Canva </p>
-        <p className="text-grey-800 py-1"> Figma </p>
+      <div
+        style={{
+          boxShadow: "1px 0px 10px 3px #d6d6e8",
+        }}
+        className=" text-center dark:shadow-slate-400 transition-shadow p-10 rounded-xl my-10  dark:text-white"
+      >
+        <div>
+          <MdOutlineDesignServices className=" m-auto" size={70} />
+          <h3 className=" text-lg font-medium pt-8 pb-2">
+            Exquisite UI Designs
+          </h3>
+          <p className="py-2">
+            Creating beautiful UI designs to satisfy your needs and the needs of
+            your clients
+          </p>
+          <h4 className="text-teal-500 py-4">Design tools I use:</h4>
+          <div className="flex  m-1 items-center space-x-6 justify-center">
+            <p className="text-grey-800 py-1"> Canva </p>
+            <p className="text-grey-800 py-1"> Figma </p>
+          </div>
+
+          <p className="text-grey-800 py-1">Paint 3D</p>
         </div>
-        
-        <p className="text-grey-800 py-1">Paint 3D</p>
-    </div>
-    </div>
+      </div>
 
+      <div
+        style={{
+          boxShadow: "1px 0px 10px 3px #d6d6e8",
+        }}
+        className=" text-center dark:shadow-slate-400 shadow-md transition-shadow p-10 rounded-xl my-10  dark:text-white"
+      >
+        <div>
+          <BsCodeSlash className=" m-auto" size={70} />
+          <h3 className=" text-lg font-medium pt-8 pb-2">Elegant Code</h3>
+          <p className="py-2">
+            You have an idea for your next web project? I can make it a reality{" "}
+          </p>
+          <h4 className="text-teal-500 py-4">Coding tools I use:</h4>
+          <p className="text-grey-800 py-1">HTML,CSS, React Js</p>
+          <p className="text-grey-800 py-1"> Wordpress, Bootstrap</p>
+        </div>
+      </div>
 
-<div style={{ 
-    boxShadow: '1px 0px 10px 3px #d6d6e8',
-}}  
-className=" text-center dark:shadow-slate-400 shadow-md transition-shadow p-10 rounded-xl my-10  dark:text-white">
-        <div >
-<BsCodeSlash className=" m-auto" size={70}/>
-       <h3 className=" text-lg font-medium pt-8 pb-2">Elegant Code</h3>
-       <p className="py-2">You have an idea for your next web project? I can make it a reality </p>
-        <h4 className="text-teal-500 py-4">Coding tools I use:</h4>
-       <p className="text-grey-800 py-1">HTML,CSS, React Js</p>
-        <p className="text-grey-800 py-1"> Wordpress, Bootstrap</p>
+      <div
+        style={{
+          boxShadow: "1px 0px 10px 3px #d6d6e8",
+        }}
+        className=" text-center ?dark:shadow-lg transition-shadow p-10 rounded-xl my-10  dark:text-white"
+      >
+        <div>
+          <HiOutlineDocumentText className=" m-auto" size={70} />
+          <h3 className=" text-lg font-medium pt-8 pb-2">
+            Professional Documentation
+          </h3>
+          <p className="py-2">
+            I can help with your next big project, collaborating and helping you
+            with your documentation
+          </p>
+          <h4 className="text-teal-500 py-4">Documents I handle:</h4>
+          <p className="text-grey-800 py-1">Requirement Document</p>
+          <p className="text-grey-800 py-1">Terms and condition</p>
+          <p className="text-grey-800 py-1">Proposal document</p>
+        </div>
+      </div>
     </div>
-    </div>
-
-    <div style={{ 
-    boxShadow: '1px 0px 10px 3px #d6d6e8',
-}} 
-    className=" text-center ?dark:shadow-lg transition-shadow p-10 rounded-xl my-10  dark:text-white">
-        <div >
-            <HiOutlineDocumentText className=" m-auto" size={70}/>
-       <h3 className=" text-lg font-medium pt-8 pb-2">Professional Documentation</h3>
-       <p className="py-2">I can help with your next big project, collaborating and helping you with your documentation</p>
-        <h4 className="text-teal-500 py-4">Documents I handle:</h4>
-       <p className="text-grey-800 py-1">Requirement Document</p>
-        <p className="text-grey-800 py-1">Terms and condition</p>
-        <p className="text-grey-800 py-1">Proposal document</p>
-    </div>
-    </div>
-    
-    </div>
-       
-    );
+  );
 }
 export default Card;
--- pages/projecs.js
+++ pages/projecs.js
@@ -1,151 +1,124 @@
 import Image from "next/image";
-import crypto from '../public/crypto.png';
-import farm from '../public/farm.png';
-import maykolly from '../public/maykolly-redesigned.png';
-import codeGenerator from '../public/codeGenerator.png';
-import commerce from '../public/tailindCommerce.png';
-import Fylo from '../public/Fylo.png'
-import abel from '../public/abel-tee.png'
-import adoption from '../public/adopt.png'
-import ecomm from '../public/ecomm.png'
-import crms from '../public/crms.png'
-import housing2 from '../public/housing.png'
-function Portfolio(){
-    return(
+import crypto from "../public/crypto.png";
+import farm from "../public/farm.png";
+import studentHub from "../public/studenthub360.png";
+import abel from "../public/abel-tee.png";
+import adoption from "../public/adopt.png";
+import housing2 from "../public/housing.png";
+function Portfolio() {
+  return (
     <div>
-        <div >
-        <h3 className=" lg:text-4xl text-3xl py-4 text-center"> <b>Projects I am proud of</b> </h3>
+      <div>
+        <h3 className=" lg:text-4xl text-3xl py-4 text-center">
+          {" "}
+          <b>Projects I am proud of</b>{" "}
+        </h3>
         <p className="text-md py-2 leading-8 text-gray-800  dark:text-white text-center">
-            These are just a few finished projects that I built from scratch with
-             <span className="text-teal-500"> beautiful UI designs, </span>
-             and a simple layout for easy navigation,
-            <span className="text-teal-500"> elegant code </span>
-           for easy maintainance and reusability, meeting the requirements of clients.
+          These are just a few finished projects that I built from scratch with
+          <span className="text-teal-500"> beautiful UI designs, </span>
+          and a simple layout for easy navigation,
+          <span className="text-teal-500"> elegant code </span>
+          for easy maintainance and reusability, meeting the requirements of
+          clients.
         </p>
-        
-       </div>
-       <div className=" flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-        
-        
-        <a href = "https://ailanthushospitalityservices.com.ng/"target="_blank" rel="noopener noreferrer">
-            <div className=" basis-1/3 flex-1">
-                <Image src={housing2} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
-        </a>
-        
-        
-        
-        <a href = "https://pet-westliam-x.vercel.app/" target="_blank" rel="noopener noreferrer">
-            <div className=" basis-1/3 flex-1">
-                <Image src={adoption} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
-        </a>
-       
-      
-       
-         {/* <a href = "http://westliam-projects.buzz/southernview/" target="_blank" rel="noopener noreferrer">
-            <div className=" basis-1/3 flex-1">
-                <Image src={codeGenerator} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
-         </a> */}
-
-  <a href = "https://ailanthusfarmproducts.com.ng/" target="_blank" rel="noopener noreferrer">
-            <div className=" basis-1/3 flex-1">
-                <Image src={farm} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
-        </a>
-{/* 
-<a href = "https://ecommerce-landing-page-6oan.vercel.app/" target="_blank" rel="noopener noreferrer">
-            <div className=" basis-1/3 flex-1">
-                <Image src={commerce} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
-        </a>
-        
-                        
-        <a href = "http://westliam-projects.buzz/queensplastic/" target="_blank" rel="noopener noreferrer">                 
-            <div className=" basis-1/3 flex-1">
-                <Image src={ecomm} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
-        </a> */}
-
-        {/* <a href="http://westliam-projects.buzz/crms/" target="_blank" rel="noopener noreferrer">
-            <div className=" basis-1/3 flex-1">
-                <Image src={crms} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
-        </a>             */}
-        
-       <a href = "https://westliam-x.github.io/crypto-landing-page/" target="_blank" rel="noopener noreferrer">
-            <div className=" basis-1/3 flex-1">     
-                <Image src={crypto} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
+      </div>
+      <div className=" flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+        <a
+          href="https://ailanthushospitalityservices.com.ng/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className=" basis-1/3 flex-1">
+            <Image
+              src={housing2}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
         </a>
 
-
-        {/* <a href="https://westliam-x.github.io/maykolly-restaurant/" target="_blank" rel="noopener noreferrer">
-            <div className=" basis-1/3 flex-1">
-                <Image src={maykolly} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
-        </a> */}
-
-        <a href="https://westliam-x.github.io/abel-tee/" target="_blank" rel="noopener noreferrer">
-            <div className=" basis-1/3 flex-1">
-                <Image src={abel} 
-                    className="rounded-lg object-cover" 
-                    width={"100%"} 
-                    height={"100%"} 
-                    layout="responsive"
-                />
-            </div>
+        <a
+          href="https://pet-westliam-x.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className=" basis-1/3 flex-1">
+            <Image
+              src={adoption}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
         </a>
-                        
-                        
-       </div>
+        <a
+          href="https://ailanthusfarmproducts.com.ng/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className=" basis-1/3 flex-1">
+            <Image
+              src={farm}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+        </a>
+
+        <a
+          href="https://www.studenthub360.software/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className=" basis-1/3 flex-1">
+            <Image
+              src={studentHub}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+        </a>
+
+        <a
+          href="https://westliam-x.github.io/crypto-landing-page/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className=" basis-1/3 flex-1">
+            <Image
+              src={crypto}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+        </a>
+
+        <a
+          href="https://westliam-x.github.io/abel-tee/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className=" basis-1/3 flex-1">
+            <Image
+              src={abel}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+        </a>
+      </div>
     </div>
-    );
+  );
 }
 export default Portfolio;
